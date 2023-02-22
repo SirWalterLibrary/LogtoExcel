fcb868b6f709f12af19f833b0feb0dd3a9cf87ab
SirWalterLibrary <mohamed.mohamed@sick.com>
2023-02-22 13:58:12 -0600

    Changed icon for fielddialog box Cleaned up imports commands Changed double to single quotes for non-messages

--- src/functions.py
+++ src/functions.py
@@ -1,13 +1,11 @@
 # import modules
 import csv
-
 import pandas as pd
 from sys import exit
-from tkinter import filedialog
+from tkinter import Tk, filedialog
 from openpyxl.worksheet import cell_range 
 from openpyxl.worksheet.table import Table, TableStyleInfo
-from openpyxl.utils import get_column_letter
-from openpyxl.utils import column_index_from_string
+from openpyxl.utils import get_column_letter, column_index_from_string
 from openpyxl.utils.dataframe import dataframe_to_rows
 
 class Data:
@@ -15,11 +13,11 @@ class Data:
     def __init__(self,index=None):
         # create empty lists
         self.data = []
-        self.headers = getHeaders("headers.txt",index)
+        self.headers = getHeaders('src/headers.txt',index)
         
     def __call__(self,other):
         # get initial headers and its range
-        initial_headers = getHeaders("headers.txt",0)
+        initial_headers = getHeaders('src/headers.txt',0)
         initial_range = [x + 1 for x in list(range(len(initial_headers)))]
         
         # split data to its respective category (i.e. Dimensions, Contour Verify, Corner)
@@ -28,7 +26,10 @@ class Data:
 
     def getData(self):
         # open dialog to browse file in File Explorer 
-        filepath = filedialog.askopenfilename(initialdir="/", 
+        win = Tk()
+        win.withdraw()
+        win.iconbitmap(r'src/log.ico')
+        filepath = filedialog.askopenfilename(initialdir='/', 
                                               title="Select a File", 
                                               filetypes=[("Log files", ".txt .log")])
         
