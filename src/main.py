726c9604023f06605e4ff8dc85e48428c5ac886f
SirWalterLibrary <mohamed.mohamed@sick.com>
2023-05-25 11:29:57 -0500

    Got main.py to function 100%

--- src/main.py
+++ src/main.py
@@ -8,13 +8,15 @@ import pandas as pd
 import tkinter as tk
 from tkinter import messagebox, Tk, filedialog
 from openpyxl import Workbook 
+from openpyxl.styles import PatternFill
+from openpyxl.formatting.rule import CellIsRule
+from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.worksheet.cell_range import CellRange 
 from openpyxl.worksheet.table import Table, TableStyleInfo
-from openpyxl.utils.dataframe import dataframe_to_rows
 
 def main():
 
-    def script(filepath):
+    def script(filepath,option=None,length=0,width=0,height=0):
         class Log:
             def __init__(self):
                 # create empty data list
@@ -28,10 +30,7 @@ def main():
                 win = Tk()
                 win.withdraw()
                 win.iconbitmap(r'src/log.ico')
-
-                # open dialog to browse file in File Explorer
-                #filepath = filedialog.askopenfilename(initialdir='/', title="Select a File", filetypes=[("Log files", ".txt .log")])
-                
+        
                 # check if user imported file 
                 if filepath != '':
                     # append file contents into data list 
@@ -79,7 +78,7 @@ def main():
                 # split data to its respective category (i.e. Dimensions, Contour Verify, Corner)
                 self.range = initial_range + [index + 1 for index, item in enumerate(other.headers) if item in self.headers]
                 self.data = (other.data[self.range]).set_axis((initial_headers + self.headers),axis=1)
-            
+
             def formatTable(self, worksheet):
                 # define range
                 full_range = CellRange(min_col=worksheet.min_column,
@@ -98,9 +97,14 @@ def main():
                 # add the table to the worksheet
                 worksheet.add_table(table)
 
-            def paste2Excel(self,worksheet,title):
+            def paste2Excel(self,worksheet,title,add_cols=None):
                 # title the worksheet
                 worksheet.title = title
+                
+                if not add_cols == None:
+                    content = [''] * len(add_cols)
+                    self.data[add_cols] = content
+                    
 
                 # paste data frame into Excel worksheet
                 for rows in dataframe_to_rows(self.data, index=False, header=True):
@@ -118,7 +122,7 @@ def main():
         # parse data from log
         log.parseData()
 
-        # initialize "Dimensions" category
+        # initialize "Results" category
         res = Category(1)
         res(log)
 
@@ -137,14 +141,28 @@ def main():
         # open a workbook
         wb = Workbook()
 
-        # create "Dimensions" sheet & paste data
+        # create "Results" sheet & paste data
         ws1 = wb.active
-        res.paste2Excel(ws1,"Results")
+        expected_cols = ['Expected L','Expected W','Expected H']
+        error_cols = ['Error L','Error W','Error H']
+        res.paste2Excel(ws1,"Results", expected_cols + error_cols)
+
+        for row_num in range(2, ws1.max_row+1):
+            ws1['L'+ str(row_num)] = '=IF(NOT(ISNUMBER(VALUE(LEFT(I'+str(row_num)+',1)))),"",F'+str (row_num)+'-I'+str (row_num)+')'
+            ws1['M'+ str(row_num)] = '=IF(NOT(ISNUMBER(VALUE(LEFT(J'+str(row_num)+',1)))),"",G'+str (row_num)+'-J'+str (row_num)+')'
+            ws1['N'+ str(row_num)] = '=IF(NOT(ISNUMBER(VALUE(LEFT(K'+str(row_num)+',1)))),"",H'+str (row_num)+'-K'+str (row_num)+')'
+        
+        if option == 1:
+            red_color = 'ffc7ce'
+            red_fill = PatternFill(start_color=red_color, end_color=red_color, fill_type='solid')
+            ws1.conditional_formatting.add('L2:L'+str(ws1.max_row), CellIsRule(operator='notBetween', formula=['-' + length,length], fill=red_fill))
+            ws1.conditional_formatting.add('M2:M'+str(ws1.max_row), CellIsRule(operator='notBetween', formula=['-' + width,width], fill=red_fill))
+            ws1.conditional_formatting.add('N2:N'+str(ws1.max_row), CellIsRule(operator='notBetween', formula=['-' + height,height], fill=red_fill))
+
 
         # create "Dimensions" sheet & paste data
         ws2 = wb.create_sheet()
         dim.paste2Excel(ws2,"Dimensions")
-
 
         # create "Contour Verify" sheet & paste data
         ws3 = wb.create_sheet()
@@ -159,16 +177,18 @@ def main():
 
         # check if "dims.xlsx" was created from log
         if exists(excel_file):
-            print("Log file successfully parsed to Excel!")
+           messagebox.showinfo(title="Success!", message="Log file successfully parsed to Excel!")
         else:
             print("ERROR: Log file unsuccessfully parsed to Excel...")
 
-    def okay():
-        len = len_entry.get()
-        wid = wid_entry.get()
-        hei = hei_entry.get()
+    def with_tol():
+        length = len_entry.get()
+        width = wid_entry.get()
+        height = hei_entry.get()
 
-        if not (type(len) == int or type(wid) == int or type(hei) == int):
+        try:
+            int(length), int(width), int(height)
+        except:
             messagebox.showerror("Error", "Please input numeric tolerances")
             return
 
@@ -186,11 +206,16 @@ def main():
             messagebox.showerror("Error","\"dims.xlsx\" is still open. You need to close it!")
             return
 
-        script(filepath)
+        try:
+            script(filepath,1,length,width,height)
+        except:
+            messagebox.showerror("Error", "Script cannot parse data. Check if log is formatted correctly!")
+            return
+        
         window.destroy()
         exit(0)
 
-    def skip():
+    def wout_tol():
         filepath = import_entry.get()
 
         if filepath == '':
@@ -203,9 +228,15 @@ def main():
         # check if excel file can be closed
         if not close_excel(excel_file):
             messagebox.showerror("Error","\"dims.xlsx\" is still open. You need to close it!")
+            f = open(excel_file, 'x')
             return
         
-        script(filepath)
+        try:
+            script(filepath)
+        except:
+            messagebox.showerror("Error", "Script cannot parse data. Check if log is formatted correctly!")
+            return
+        
         window.destroy()
         exit(0)
 
@@ -219,16 +250,20 @@ def main():
     def close_excel(excel_file):
 
         # remove if "dims.xlsx" exists
-        if exists(excel_file):
+        if not exists(excel_file):
+            f = open(excel_file, 'x')
+            return True
+        else:
             try:
                 os.remove(excel_file)
                 return True
-            except OSError:  
+            except:
                 return False
 
     # specify excel file name
     excel_file = 'dims.xlsx'
 
+    # create a UI window
     window = tk.Tk()
     window.title("Input Log Data")
     window.geometry("500x210")
@@ -236,45 +271,49 @@ def main():
     frame = tk.Frame(window)
     frame.pack()
 
-    # Input Tolerance Frame
+    # declare an "Input Tolerance" frame
     input_tol_frame = tk.LabelFrame(frame,text="Input Tolerance")
     input_tol_frame.grid(row=0, column=0, sticky="ns", pady=10)
 
+    # create an entry for "Length"
     len_label = tk.Label(input_tol_frame,text="Length")
     len_label.grid(row=0,column=0)
     len_entry = tk.Entry(input_tol_frame)
     len_entry.grid(row=1, column=0)
 
+    # create an entry for "Width"
     wid_label = tk.Label(input_tol_frame,text="Width")
     wid_label.grid(row=0, column=1)
     wid_entry = tk.Entry(input_tol_frame)
     wid_entry.grid(row=1, column=1)
 
+    # create an entry for "Height"
     hei_label = tk.Label(input_tol_frame,text="Height")
     hei_label.grid(row=0,column=2)
     hei_entry = tk.Entry(input_tol_frame)
     hei_entry.grid(row=1,column=2)
 
-    # Export To Frame
-    
+    # declare an "Input File" frame
     import_frame = tk.LabelFrame(frame, borderwidth=0)
     import_frame.grid(row=1, column=0)
 
+    # create an "input File" button
     import_entry = tk.Entry(import_frame, width=50)
     import_entry.grid(row=0, column=1)
     import_button = tk.Button(import_frame, text="Import File", command=lambda:import_file())
     import_button.grid(row=0, column=0)
 
-    # Confirm/Cancel Button
+    # declare a "Run with" frame
     validate_frame = tk.LabelFrame(frame, text="Run with or without tolerances?")
     validate_frame.grid(row=3, column=0)
 
-    ok_button = tk.Button(validate_frame, text="With", width=20, command=okay)
-    ok_button.grid(row=0, column=0)
-    skip_button = tk.Button(validate_frame, text="Without", width=20, command=skip)
-    skip_button.grid(row=0, column=1)
+    # create button for "With" & "Without" tolerances
+    with_button = tk.Button(validate_frame, text="With", width=20, command=with_tol)
+    with_button.grid(row=0, column=0)
+    wout_button = tk.Button(validate_frame, text="Without", width=20, command=wout_tol)
+    wout_button.grid(row=0, column=1)
 
-    # Adjust Padding
+    # adjust padding
     for widget in input_tol_frame.winfo_children():
         widget.grid_configure(padx=10, pady=5)
 
@@ -284,6 +323,7 @@ def main():
     for widget in validate_frame.winfo_children():
         widget.grid_configure(padx=30, pady=10)
 
+    # end code with loop
     window.mainloop()
 
 if __name__ == "__main__":
