7eec3005e2517245537a1286c142916441a1103e
SirWalterLibrary <mohamed.mohamed@sick.com>
2023-08-22 11:53:11 -0500

    Optimizing code, putting methods away from main()

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # summary: main.py gets dim log and outputs an Excel table formatted for analysis
  
 # import modules
-import sys, os, csv
+import sys, os, csv, shutil
 from sys import exit
 from os.path import exists
 import pandas as pd
@@ -17,232 +17,214 @@ from openpyxl.worksheet.table import Table, TableStyleInfo
 log_path = None
 excel_path = None
 
-def main():
+def resource_path(relative_path):
+    # get absolute path to resource
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
 
-    def resource_path(relative_path):
-        # get absolute path to resource
-        base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-        return os.path.join(base_path, relative_path)
+def download_file():
+    src = r"C:/Users/mohammo/Documents/Projects/Dims Template Code/Python/LogtoExcel/Dimension_Expression_Editor.xml"
+    des = filedialog.asksaveasfilename(title="Download", initialdir="Downloads", initialfile="Dimension_Expression_Editor.xml",  filetypes=[("Expression Editor",".xml")])
+    try: 
+        shutil.copyfile(src,des)
+    except:
+        return
+    return
 
-    def script(log_path,length=0,width=0,height=0):
-        # retrieve excel file path
-        excel_file = export_entry.get()
+def script(log_path,export_entry,length=0,width=0,height=0):
+    # retrieve excel file path
+    excel_file = export_entry.get()
 
-        class Log:
-            def __init__(self):
-                # create empty data list
-                self.data = []
+    class Log:
+        def __init__(self):
+            # create empty data list
+            self.data = []
 
-                # get headers from headers.txt
-                self.headers = self.getHeaders()
-                
-            def getData(self):
-        
-                # check if user imported file 
-                if log_path != '':
-                    # append file contents into data list 
-                    with open(log_path) as logFile:
-                        for row in csv.reader(logFile, delimiter=';'):
-                            self.data.append(row)
+            # get headers from headers.txt
+            self.headers = self.getHeaders()
+            
+        def getData(self):
+    
+            # check if user imported file 
+            if log_path != '':
+                # append file contents into data list 
+                with open(log_path) as logFile:
+                    for row in csv.reader(logFile, delimiter=';'):
+                        self.data.append(row)
+            else:
+                # error if no file was imported
+                print("No log file was imported")
+                exit()
+
+        def parseData(self):
+            # create data frame and parse through data
+            self.getData()
+            self.data = pd.DataFrame(self.data).iloc[:,1:-1].dropna()
+
+        def getHeaders(self, line_number=None):
+            # specify header file
+            filename = resource_path("headers.txt")
+
+            # open header file
+            with open(filename) as file:
+                # get all headers
+                if line_number is None:
+                    return [x.strip(' ') for x in (" ".join(line.strip() for line in file)).split(';')]
                 else:
-                    # error if no file was imported
-                    print("No log file was imported")
-                    exit()
+                    for i, line in enumerate(file):
+                        # get only the headers from the line
+                        if i == line_number:
+                            return list(filter(None,(line.strip()).split(';')))
 
-            def parseData(self):
-                # create data frame and parse through data
-                self.getData()
-                self.data = pd.DataFrame(self.data).iloc[:,1:-1].dropna()
+    class Category(Log):
 
-            def getHeaders(self, line_number=None):
-                # specify header file
-                filename = resource_path("headers.txt")
+        def __init__(self, index=None):
+            # calls original __init__()
+            super().__init__()
+            self.headers = self.getHeaders(index)
 
-                # open header file
-                with open(filename) as file:
-                    # get all headers
-                    if line_number is None:
-                        return [x.strip(' ') for x in (" ".join(line.strip() for line in file)).split(';')]
-                    else:
-                        for i, line in enumerate(file):
-                            # get only the headers from the line
-                            if i == line_number:
-                                return list(filter(None,(line.strip()).split(';')))
+        def __call__(self,other):
+            # get initial headers and its range
+            initial_headers = self.getHeaders(0)
+            initial_range = [x + 1 for x, header in enumerate(initial_headers)]
+            
+            # split data to its respective category (i.e. Dimensions, Contour Verify, Corner)
+            self.range = initial_range + [index + 1 for index, item in enumerate(other.headers) if item in self.headers]
+            self.data = (other.data[self.range]).set_axis((initial_headers + self.headers),axis=1)
 
-        class Category(Log):
+        def formatTable(self, worksheet):
+            # define range
+            full_range = CellRange(min_col=worksheet.min_column,
+                                    min_row=worksheet.min_row,
+                                    max_col=worksheet.max_column,
+                                    max_row=worksheet.max_row).coord
 
-            def __init__(self, index=None):
-                # calls original __init__()
-                super().__init__()
-                self.headers = self.getHeaders(index)
+            # set table format
+            mediumStyle =TableStyleInfo(name='TableStyleMedium1',
+                                        showRowStripes=True)
+            # create a table
+            table = Table(ref=full_range,
+                            displayName=(worksheet.title).replace(" ", "_"),
+                            tableStyleInfo=mediumStyle)
 
-            def __call__(self,other):
-                # get initial headers and its range
-                initial_headers = self.getHeaders(0)
-                initial_range = [x + 1 for x, header in enumerate(initial_headers)]
-                
-                # split data to its respective category (i.e. Dimensions, Contour Verify, Corner)
-                self.range = initial_range + [index + 1 for index, item in enumerate(other.headers) if item in self.headers]
-                self.data = (other.data[self.range]).set_axis((initial_headers + self.headers),axis=1)
+            # add the table to the worksheet
+            worksheet.add_table(table)
 
-            def formatTable(self, worksheet):
-                # define range
-                full_range = CellRange(min_col=worksheet.min_column,
-                                       min_row=worksheet.min_row,
-                                       max_col=worksheet.max_column,
-                                       max_row=worksheet.max_row).coord
+        def paste2Excel(self,worksheet,title,add_cols=None):
+            # title the worksheet
+            worksheet.title = title
+            
+            if not add_cols == None:
+                content = [''] * len(add_cols)
+                self.data[add_cols] = content 
 
-                # set table format
-                mediumStyle =TableStyleInfo(name='TableStyleMedium1',
-                                            showRowStripes=True)
-                # create a table
-                table = Table(ref=full_range,
-                              displayName=(worksheet.title).replace(" ", "_"),
-                              tableStyleInfo=mediumStyle)
+            # paste data frame into Excel worksheet
+            for rows in dataframe_to_rows(self.data, index=False, header=True):
+                worksheet.append(rows)
 
-                # add the table to the worksheet
-                worksheet.add_table(table)
+            # format table from data
+            self.formatTable(worksheet)
+    
+    # initialize log
+    log = Log()
 
-            def paste2Excel(self,worksheet,title,add_cols=None):
-                # title the worksheet
-                worksheet.title = title
-                
-                if not add_cols == None:
-                    content = [''] * len(add_cols)
-                    self.data[add_cols] = content 
+    # parse data from log
+    log.parseData()
 
-                # paste data frame into Excel worksheet
-                for rows in dataframe_to_rows(self.data, index=False, header=True):
-                    worksheet.append(rows)
+    # initialize "Results" category
+    res = Category(1)
+    res(log)
 
-                # format table from data
-                self.formatTable(worksheet)
-        
-        # initialize log
-        log = Log()
+    # initialize "Dimensions" category
+    dim = Category(2)
+    dim(log)
 
-        # parse data from log
-        log.parseData()
+    # initialize "Contour Verify" category
+    c_v = Category(3)
+    c_v(log)
 
-        # initialize "Results" category
-        res = Category(1)
-        res(log)
+    # open a workbook
+    wb = Workbook()
 
-        # initialize "Dimensions" category
-        dim = Category(2)
-        dim(log)
+    # create "Results" sheet & paste data
+    ws1 = wb.active
+    expected_cols = ['Expected L','Expected W','Expected H']
+    error_cols = ['Error L','Error W','Error H']
+    res.paste2Excel(ws1,"Results", expected_cols + error_cols)
 
-        # initialize "Contour Verify" category
-        c_v = Category(3)
-        c_v(log)
+    # create a formula to find difference between measured & expected dimensions
+    for row_num in range(2, ws1.max_row+1):
+        ws1['T'+ str(row_num)] = '=IF(NOT(ISNUMBER(VALUE(LEFT(Q'+str(row_num)+',1)))),"",F'+str (row_num)+'-Q'+str (row_num)+')'
+        ws1['U'+ str(row_num)] = '=IF(NOT(ISNUMBER(VALUE(LEFT(R'+str(row_num)+',1)))),"",G'+str (row_num)+'-R'+str (row_num)+')'
+        ws1['V'+ str(row_num)] = '=IF(NOT(ISNUMBER(VALUE(LEFT(S'+str(row_num)+',1)))),"",H'+str (row_num)+'-S'+str (row_num)+')'
+    
+    # color cells red if error is out of spec
+        red_color = 'ffc7ce'
+        red_fill = PatternFill(start_color=red_color, end_color=red_color, fill_type='solid')
+        ws1.conditional_formatting.add('T2:T'+str(ws1.max_row), CellIsRule(operator='notBetween', formula=['-' + length,length], fill=red_fill))
+        ws1.conditional_formatting.add('U2:U'+str(ws1.max_row), CellIsRule(operator='notBetween', formula=['-' + width,width], fill=red_fill))
+        ws1.conditional_formatting.add('V2:V'+str(ws1.max_row), CellIsRule(operator='notBetween', formula=['-' + height,height], fill=red_fill))
 
-        # open a workbook
-        wb = Workbook()
+    # create "Dimensions" sheet & paste data
+    ws2 = wb.create_sheet()
+    dim.paste2Excel(ws2,"Dimensions")
 
-        # create "Results" sheet & paste data
-        ws1 = wb.active
-        expected_cols = ['Expected L','Expected W','Expected H']
-        error_cols = ['Error L','Error W','Error H']
-        res.paste2Excel(ws1,"Results", expected_cols + error_cols)
+    # create "Contour Verify" sheet & paste data
+    ws3 = wb.create_sheet()
+    c_v.paste2Excel(ws3,"Contour Verify")
 
-        # create a formula to find difference between measured & expected dimensions
-        for row_num in range(2, ws1.max_row+1):
-            ws1['T'+ str(row_num)] = '=IF(NOT(ISNUMBER(VALUE(LEFT(Q'+str(row_num)+',1)))),"",F'+str (row_num)+'-Q'+str (row_num)+')'
-            ws1['U'+ str(row_num)] = '=IF(NOT(ISNUMBER(VALUE(LEFT(R'+str(row_num)+',1)))),"",G'+str (row_num)+'-R'+str (row_num)+')'
-            ws1['V'+ str(row_num)] = '=IF(NOT(ISNUMBER(VALUE(LEFT(S'+str(row_num)+',1)))),"",H'+str (row_num)+'-S'+str (row_num)+')'
-        
-        # color cells red if error is out of spec
-            red_color = 'ffc7ce'
-            red_fill = PatternFill(start_color=red_color, end_color=red_color, fill_type='solid')
-            ws1.conditional_formatting.add('T2:T'+str(ws1.max_row), CellIsRule(operator='notBetween', formula=['-' + length,length], fill=red_fill))
-            ws1.conditional_formatting.add('U2:U'+str(ws1.max_row), CellIsRule(operator='notBetween', formula=['-' + width,width], fill=red_fill))
-            ws1.conditional_formatting.add('V2:V'+str(ws1.max_row), CellIsRule(operator='notBetween', formula=['-' + height,height], fill=red_fill))
+    # save workbook
+    wb.save(excel_file)
 
-        # create "Dimensions" sheet & paste data
-        ws2 = wb.create_sheet()
-        dim.paste2Excel(ws2,"Dimensions")
+    # check if excel was created from log
+    if exists(excel_file):
+        messagebox.showinfo(title="Success!", message="Log file successfully parsed to Excel!")
+        os.startfile(excel_file)
+    else:
+        print("ERROR: Log file unsuccessfully parsed to Excel...")
 
-        # create "Contour Verify" sheet & paste data
-        ws3 = wb.create_sheet()
-        c_v.paste2Excel(ws3,"Contour Verify")
+def run(len_entry,wid_entry,hei_entry,window,import_entry):
+    length = len_entry.get()
+    width = wid_entry.get()
+    height = hei_entry.get()
 
-        # save workbook
-        wb.save(excel_file)
+    # ensure that user inputs tolerances
+    try:
+        int(length), int(width), int(height)
+    except:
+        messagebox.showerror("Error", "Please input numeric tolerances")
+        return
 
-        # check if excel was created from log
-        if exists(excel_file):
-           messagebox.showinfo(title="Success!", message="Log file successfully parsed to Excel!")
-           os.startfile(excel_file)
-        else:
-            print("ERROR: Log file unsuccessfully parsed to Excel...")
-
-    def with_tol():
-        length = len_entry.get()
-        width = wid_entry.get()
-        height = hei_entry.get()
-
-        # ensure that user inputs tolerances
-        try:
-            int(length), int(width), int(height)
-        except:
-            messagebox.showerror("Error", "Please input numeric tolerances")
-            return
-
-        # 
-        log_path = import_entry.get()
-        
-        if log_path == '':
-            messagebox.showerror("Error", "You must input a log file!")
-            return
-        elif not os.path.exists(log_path):
-            messagebox.showerror("Error","log_path does not exist...")
-            return
-        
-        # check if excel file can be closed
-        if not close_excel():
-
-            messagebox.showerror("Error","\"dims.xlsx\" is still open. You need to close it!")
-            return
-
-        try:
-            script(log_path,length,width,height)
-        except:
-            messagebox.showerror("Error", "Script cannot parse data. Check if log is formatted correctly!")
-            return
-        
-        window.destroy()
-        exit(0)
-
-    def import_file():
-        log_path = filedialog.askopenfilename(title="Select a File",filetypes=[("Log files", ".txt .log")])
-        
-        import_entry.delete(0,"end")
-        import_entry.insert(0,log_path)
+    # 
+    log_path = import_entry.get()
+    
+    if log_path == '':
+        messagebox.showerror("Error", "You must input a log file!")
+        return
+    elif not os.path.exists(log_path):
+        messagebox.showerror("Error","log_path does not exist...")
         return
     
-    def export_file():
-       # specify excel file path
-       excel_path = filedialog.asksaveasfilename(title="Save as", filetypes=[("Excel File",".xlsx")])
-       
-       # add ".xlsx" to the name of excel file
-       export_entry.delete(0,"end")
-       export_entry.insert(0,excel_path + '.xlsx')
-       return
-        
-    def close_excel():
-        # specify excel file name
-        excel_file = export_entry.get()
+    window.destroy()
+    exit(0)
 
-        # remove if "dims.xlsx" exists
-        if not exists(excel_file):
-            f = open(excel_file, 'x')
-            return True
-        else:
-            try:
-                os.remove(excel_file)
-                return True
-            except:
-                return False
+def import_file(import_entry):
+    log_path = filedialog.askopenfilename(title="Select a File",filetypes=[("Log files", ".txt .log")])
+    
+    import_entry.delete(0,"end")
+    import_entry.insert(0,log_path)
+    return
+
+def export_file(export_entry):
+    # specify excel file path
+    excel_path = filedialog.asksaveasfilename(title="Save as", filetypes=[("Excel File",".xlsx")], defaultextension=".xlsx")
+    
+    # add ".xlsx" to the name of excel file
+    export_entry.delete(0,"end")
+    export_entry.insert(0,excel_path)
+    return
+    
+def main():
 
     # create a UI window
     window = tk.Tk()
@@ -281,21 +263,25 @@ def main():
     # create an "Input File" button
     import_entry = tk.Entry(file_frame, width=50)
     import_entry.grid(row=0, column=1)
-    import_button = tk.Button(file_frame, text="Import file", width= 8, command=lambda:import_file())
+    import_button = tk.Button(file_frame, text="Import file", width= 8, command=lambda:import_file(import_entry))
     import_button.grid(row=0, column=0)
 
     # create an "Output Excel" button
     export_entry = tk.Entry(file_frame, width=50)
     export_entry.grid(row=1, column=1)
-    export_button = tk.Button(file_frame, text="Save as", width= 8, command=lambda:export_file())
+    export_button = tk.Button(file_frame, text="Save as", width= 8, command=lambda:export_file(export_entry))
     export_button.grid(row=1, column=0)
+
+    # create a "Download Expression Editor" button
+    download_button = tk.Button(file_frame, text="Download", width= 50, command=lambda:download_file())
+    download_button.grid(row=2, column=1)
 
     # declare a "Run with" frame
     validate_frame = tk.LabelFrame(frame, text="Run the script?")
     validate_frame.grid(row=3, column=0)
 
-    # create button for "With" & "Without" tolerances
-    with_button = tk.Button(validate_frame, text="Run", width=40, command=with_tol)
+    # create a "Run" button 
+    with_button = tk.Button(validate_frame, text="Run", width=40, command=run(len_entry,wid_entry,hei_entry,window,import_entry))
     with_button.grid(row=0, column=0)
 
     # adjust padding
